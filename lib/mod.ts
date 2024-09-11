e62d92ac0d81b803d05ce8ff1ce0791f3bd57f8f
Jiří Pavlovský <jiri@prosyn.org>
2024-09-11 17:26:49 +0200

    Initial code commit

---
+++ lib/mod.ts
@@ -0,0 +1,9 @@
+console.log("tady1");
+
+export default function getData() {
+
+  console.log("tady2");
+
+  return {key: 1};
+
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compress: false,
+  output: "standalone",
+  poweredByHeader: false,
+  compiler: {
+    removeConsole: false,
+  }
+};
 
 export default nextConfig;
